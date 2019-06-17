02beeeb6dfa89a5e60f0deeee180cfed26c849d0
Alex Lang <langalex4@gmail.com>
2019-06-17 09:40:20 -0500

    Flesh out Shipment class

--- lib/shipment.rb
+++ lib/shipment.rb
@@ -1,2 +1,64 @@
+require 'date'
+
 class Shipment
+
+  attr_reader :first_name, :last_name, :number, :order_number, :parent_number,
+    :shipment_date
+
+  def initialize(attributes)
+    @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @number = attributes.fetch(:number)
+    @order_number = attributes.fetch(:order_number)
+    @parent_number = attributes.fetch(:parent_number, "N/A")
+    @shipment_date = attributes.fetch(:shipment_date)
+  end
+
+  def to_str
+    [
+      number_string,
+      order_number_string,
+      shipment_date_string,
+      first_name_string,
+      last_name_string,
+      parent_shipment_string,
+    ].join(", ")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def days_ago
+    today = Date.today
+    shipped = DateTime.parse(shipment_date).to_date
+    (today - shipped).to_i
+  end
+
+  private
+
+  def number_string
+    "Number: #{number}"
+  end
+
+  def order_number_string
+    "Order Number: #{order_number}"
+  end
+
+  def shipment_date_string
+    "Shipped: #{shipment_date}"
+  end
+
+  def first_name_string
+    "First Name: #{first_name}"
+  end
+
+  def last_name_string
+    "Last Name: #{last_name}"
+  end
+
+  def parent_shipment_string
+    "Parent Shipment: #{parent_number}"
+  end
 end
+
--- spec/shipment_spec.rb
+++ spec/shipment_spec.rb
@@ -1,4 +1,108 @@
 require 'rspec'
+require 'shipment'
+require 'date'
+require 'timecop'
 
 describe Shipment do
+  let(:full_attributes) do
+    {
+      first_name:    "Ben",
+      last_name:     "Wyatt",
+      number:        "SH432123",
+      order_number:  "987654",
+      shipment_date: "2018-12-10 15:08:58 -0000",
+      parent_number: "N/A"
+    }
+  end
+
+  let(:attributes_with_parent_order) do
+    full_attributes.merge({ parent_number: "SH86739" })
+  end
+
+
+  describe "#initialize" do
+    context "with no parent number provided" do
+      it "sets the non-shipment_date attributes" do
+        shipment = Shipment.new(full_attributes)
+
+        full_attributes.each do |key, value|
+          expect(shipment.public_send(key)).to eq(value)
+        end
+      end
+    end
+
+    context "with a parent number provided" do
+      it "sets the non-shipment_date attributes" do
+        shipment = Shipment.new(attributes_with_parent_order)
+
+        attributes_with_parent_order.each do |key, value|
+          expect(shipment.public_send(key)).to eq(value)
+        end
+      end
+    end
+  end
+
+  describe "#to_str" do
+    context "without a parent shipment" do
+      it "returns a formatted string of the shipment's attributes" do
+        shipment = Shipment.new(full_attributes)
+        expect(shipment.to_str).to eq(
+          "Number: #{full_attributes[:number]}, "\
+          "Order Number: #{full_attributes[:order_number]}, "\
+          "Shipped: #{full_attributes[:shipment_date]}, "\
+          "First Name: #{full_attributes[:first_name]}, "\
+          "Last Name: #{full_attributes[:last_name]}, "\
+          "Parent Shipment: N/A"
+        )
+      end
+    end
+
+    context "with a parent shipment" do
+      it "returns a formatted string of the shipment's attributes" do
+        shipment = Shipment.new(attributes_with_parent_order)
+        expect(shipment.to_str).to eq(
+          "Number: #{attributes_with_parent_order[:number]}, "\
+          "Order Number: #{attributes_with_parent_order[:order_number]}, "\
+          "Shipped: #{attributes_with_parent_order[:shipment_date]}, "\
+          "First Name: #{attributes_with_parent_order[:first_name]}, "\
+          "Last Name: #{attributes_with_parent_order[:last_name]}, "\
+          "Parent Shipment: #{attributes_with_parent_order[:parent_number]}"
+        )
+      end
+    end
+  end
+
+  describe "#full_name" do
+    it "returns the customer's first and last names" do
+      shipment = Shipment.new(full_attributes)
+      expect(shipment.full_name).to eq(
+        "#{full_attributes[:first_name]} #{full_attributes[:last_name]}"
+      )
+    end
+  end
+
+  describe "#days_ago" do
+    let(:shipment_date) { DateTime.parse(full_attributes[:shipment_date]) }
+
+    context "when no days have passed" do
+      it "returns 0" do
+        shipment = Shipment.new(full_attributes)
+
+        Timecop.freeze(shipment_date) do
+          expect(shipment.days_ago).to eq(0)
+        end
+      end
+    end
+
+    context "when some days have passed" do
+      it "returns the number of days since the shipment was shipped" do
+        shipment = Shipment.new(full_attributes)
+        days = 25
+
+        Timecop.freeze(shipment_date + days) do
+          expect(shipment.days_ago).to eq(days)
+        end
+      end
+    end
+  end
 end
